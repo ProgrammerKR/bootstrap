449c2830d5bc630514159f16674e7988f0cf8d3a
Prog. Kanishk Raj <programmerkr.123@gmail.com>
2025-04-16 19:57:01 +0530

    Update package.js

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
-// package metadata file for Meteor.js
+// package metadata file for ProXtyle
 
 /* eslint-env meteor */
 
 Package.describe({
-  name: 'twbs:bootstrap', // https://atmospherejs.com/twbs/bootstrap
-  summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
-  version: '5.3.3',
-  git: 'https://github.com/twbs/bootstrap.git'
+  name: 'proxstyle', // Your package name
+  summary: 'A high-performance CSS framework for building responsive, mobile-first web designs.',
+  version: '1.0.0', // Adjust the version as necessary
+  git: 'https://github.com/ProgrammerKR/ProXtyle.git' // Your GitHub URL for ProXtyle
 })
 
 Package.onUse(api => {
   api.versionsFrom('METEOR@1.0')
   api.addFiles([
-    'dist/css/bootstrap.css',
-    'dist/js/bootstrap.js'
+    'dist/css/proxstyle.css', // Path to your compiled CSS file
+    'dist/js/proxstyle.js' // If you have JavaScript functionalities for ProXtyle
   ], 'client')
 })
